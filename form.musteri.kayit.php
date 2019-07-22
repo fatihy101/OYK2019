5e8e696f981834f3644d968bd6fdc6b7a7f153fe
Fatih Yaman <fatihy@yahoo.com>
2019-07-22 17:21:36 +0300

    "acadam" projesinin taslak logini yüklendi.

---
+++ form.musteri.kayit.php
@@ -0,0 +1,70 @@
+<head>
+
+</head>
+
+
+<body>
+
+<h1>Aç Adam</h1>
+
+
+<div>
+
+<forms>
+<p>Adınız: </p>
+<input type="text" name="ad" value="">
+<p>Soyadınız:</p>
+<input type="text" name="soyad" value="">
+<p>Telefon numarası:</p>
+<input type="tel" name="telno" value="">
+<p>E-posta:</p>
+<input type="email" name="eposta" value="">
+<p>Parola:</p>
+<input type="password" name="parola" value="">
+
+
+<h3>Adres Bilgisi</h3>
+<p>İl:</p>
+<select name= "il">
+  <option value="İl1"></option>
+  <option value="İl2"></option>
+</select>
+<p>İlçe</p>
+<select name="ilçe">
+  <option value="İlçe1"></option>
+  <p>İlçe:</p>
+  <option value="İlçe2"></option>
+  <option value="ilçe3"></option>
+</select>
+<p>Mahalle:</p>
+<select  name="mahalle">
+  <option value="Mahalle1"></option>
+  <option value="Mahalle2"></option>
+  <option value="Mahalle3"></option>
+</select>
+
+<p>Cadde:</p>
+<input type="text" name="cadde" value="">
+<p>Sokak:</p>
+<input type="text" name="sokak" value="">
+<p>Bina No:</p>
+<input type="text" name="bina_no" value="">
+<p>Daire No:</p>
+<input type="text" name="daire_no" value="">
+<br />
+<p><input type="checkbox" name="userAgreement" value=""><a href="#">Kullanıcı sözleşmesi</a>ni okudum ve kabul ediyorum.
+
+</p>
+<button type="submit" name="button">Kayıt Ol</button>
+</forms>
+
+</div>
+
+<style>
+body{ margin-left:40em;margin-bottom:150px;margin-top:100px}
+
+
+
+</style>
+
+</body>
